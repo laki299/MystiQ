c5d4871cf5a31cc8bcacfe05b7602e793c0f50a9
laki299 <laki35083@gmail.com>
2026-09-07 00:13:12 +0600

    Add application configuration settings

---
+++ src/config/app.config.ts
@@ -0,0 +1,16 @@
+export const APP_CONFIG = {
+  appName: 'MystiQ',
+  version: '1.0.0',
+  limits: {
+    textExpirySeconds: 150, // 2 minutes 30 seconds
+    voiceDailyLimit: 25,
+    voiceMaxDurationSeconds: 60,
+    presenceHeartbeatSeconds: 25,
+    inactiveThresholdDays: 30,
+    autoSweepThreshold: 30
+  },
+  reward: {
+    durationHours: 8
+  }
+};
+
