34114ffc6ffad22edd3eaf78f22824541c221d06
laki299 <laki35083@gmail.com>
2026-09-07 02:06:15 +0600

    Refactor vite.config.ts by removing build options
    Removed build configuration and updated import statement.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs.plugin-react';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext',
-    minify: 'terser',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/database']
-        }
-      }
-    }
-  }
 });
