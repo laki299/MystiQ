d30d49ece5c91af67d378a8635c31df64114b87a
laki299 <laki35083@gmail.com>
2026-09-08 09:04:24 +0600

    Fix import statement for useExpiredCounter hook

--- src/components/profile/ProfileScreen.tsx
+++ src/components/profile/ProfileScreen.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { UserProfile } from '../../types/user.types';
 import { ProfileEditModal } from './ProfileEditModal';
 import { BackButton } from '../common/BackButton';
-importnter';
+import { useExpiredCounter } from '../../hooks/useExpiredCounter';
 
 interface ProfileScreenProps {
   profile: UserProfile;
