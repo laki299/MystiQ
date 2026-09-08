de1bb61394c7a3462fc9da50db19bd5f8c059f5b
laki299 <laki35083@gmail.com>
2026-09-08 09:13:05 +0600

    Update DiscoverScreen.tsx

--- src/components/discover/DiscoverScreen.tsx
+++ src/components/discover/DiscoverScreen.tsx
@@ -9,8 +9,7 @@ import { BackButton } from '../common/BackButton';
 
 interface DiscoverScreenProps {
   profile: UserProfile;
-  onBack?: () => void;
-}
+  onBac
 
 export const DiscoverScreen: React.FC<DiscoverScreenProps> = ({ profile, onBack }) => {
   const [categoryId, setCategoryId] = useState(APP_CONFIG.categories[0].id);
