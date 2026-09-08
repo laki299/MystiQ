a3058f53b6dd904e657c7000997201bd37022a28
laki299 <laki35083@gmail.com>
2026-09-08 09:26:41 +0600

    Remove ChatRequest and ChatRequestStatus types

--- src/types/request.types.ts
+++ src/types/request.types.ts
@@ -1,13 +1,1 @@
-export type ChatRequestStatus = 'pending' | 'accepted' | 'rejected' | 'expired';
-
-export interface ChatRequest {
-  id: string;
-  fromUid: string;
-  toUid: string;
-  categoryId: string;
-  fromName: string;
-  fromAvatar: string;
-  createdAt: number;
-  expiresAt: number;
-  status: ChatRequestStatus;
-}
+hereচ
