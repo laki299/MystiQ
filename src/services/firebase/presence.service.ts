ca3c53f9af150c9bf41e8e262ec3ae5ba2a8265d
laki299 <laki35083@gmail.com>
2026-09-08 09:22:37 +0600

    Fix import path for PresenceUser type

--- src/services/firebase/presence.service.ts
+++ src/services/firebase/presence.service.ts
@@ -1,7 +1,7 @@
 import { ref, set, onDisconnect, onValue, remove } from 'firebase/database';
 import { rtdb } from '../../config/firebase.config';
 import { UserProfile } from '../../types/user.types';
-import { PresenceUser } from '../../types/room.types';
+import { PresenceUser } from '../../';
 import { APP_CONFIG } from '../../config/app.config';
 
 export const joinCategoryPresence = async (
